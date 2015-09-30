126f39ae8d08de0a5cb50fb08010264a447d8317
Pranesha Bunsee <pbunsee@gmail.com>
2015-09-30 17:30:45 -0400

    HTML scraper using Python

---
+++ README.md
@@ -0,0 +1,13 @@
+HTML scraper written in Python
+Output is sent to stdout (i.e. on the command line)
+
+To execute on MAC OSX, issue the following command in terminal:
+
+	python scrapeStocks.py
+
+Sample output:
+	The price of aapl  is  ['110.30']
+	The price of spy  is  ['191.63']
+
+
+
---
+++ scrapeStocks.py
@@ -0,0 +1,16 @@
+import urllib
+import re
+
+symbolslist = ["aapl","spy"]
+
+i=0
+while i < len(symbolslist):
+    url = "http://finance.yahoo.com/q?s=" + symbolslist[i] + "&ql=1"
+    htmlfile = urllib.urlopen(url)
+    htmltext = htmlfile.read()
+    regex = '<span id="yfs_l84_' + symbolslist[i] + '">(.+?)</span>'
+    pattern = re.compile(regex)
+    price = re.findall(pattern,htmltext)
+    print "The price of",symbolslist[i],"is",price
+    i+=1
+
